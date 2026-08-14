2c70c51723e670a599982fb753f98b4e73df13e0
aboodking-ak <aboodgithub@gmail.com>
2026-08-14 04:10:38 +0300

    Disable Screenshot in APP

--- android/app/src/main/java/com/purecompany/sanad/MainActivity.java
+++ android/app/src/main/java/com/purecompany/sanad/MainActivity.java
@@ -1,6 +1,14 @@
 package com.purecompany.sanad;
 
+import android.os.Bundle;
+import android.view.WindowManager;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // منع أخذ لقطات الشاشة أو تسجيل الشاشة
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    }
 }
